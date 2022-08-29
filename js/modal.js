7941a06863cac0b05c867bf7fe2e40f24c0ea201
LexBondarchyk <alex_bondarchyck@ukr.net>
2022-08-29 21:32:54 +0300

    зміна модалки та шрифта

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,21 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector(newFunction()),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-  function newFunction() {
-    return "[data-modal-close]";
-  }
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector(newFunction()),
+    backdrop: document.querySelector('[data-backdrop]'),
+    modal: document.querySelector("[data-modal]"),
+  };
 
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+function newFunction() {
+  return "[data-modal-close]";
+}
+
+  function toggleModal() {
+    document.body.classList.toggle('modal-open');
+    refs.backdrop.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('modal--active');
+  }
+})();
